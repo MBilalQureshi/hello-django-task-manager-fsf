d86564233a9a239a172473706cb79ea460c47514
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2023-10-10 16:51:15 +0000

    Remove secret key and ad as config vars in gitpod and heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-as%02y0oac*ai)3hscm+r!7jouuy@dyuw$e%&!#csv^ja6qvbs')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 '''
